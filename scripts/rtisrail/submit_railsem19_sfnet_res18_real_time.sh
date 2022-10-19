1de89dbb47c6bd5c896734cf862b4abe10d6aa09
pauls3 <paulstanik3@gmail.com>
2022-10-19 14:08:35 -0700

    setting up test run

---
+++ scripts/rtisrail/submit_railsem19_sfnet_res18_real_time.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# echo "Running inference on" ${1}
+# echo "Saving Results :" ${2}
+# --snapshot pretrained_models/rs19_4000_epoch_274_mean-iu_0.57821.pth
+
+python3 eval.py \
+    --dataset rtisrail22 \
+    --arch network.sfnet_resnet.DeepR18_SF_deeply \
+    --inference_mode whole \
+    --scales 1 \
+    --split test \
+    --ckpt_path logs/pretrained_railsem19_test_0 \
+    --snapshot pretrained_models/rtisrail22_mean-iu_0.66664.pth
