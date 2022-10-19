1bbbc18be91e927036e6a857060c2fdd4a4c973f
pauls3 <paulstanik3@gmail.com>
2022-10-19 01:37:59 -0700

    test

--- datasets/uniform.py
+++ datasets/uniform.py
@@ -176,6 +176,7 @@ def build_epoch(imgs, centroids, num_classes, class_uniform_pct):
 
     # now add uniform sampling
     print(num_classes)
+    print(centroids)
     for class_id in range(num_classes):
         print((class_id, len(centroids[class_id])))
         string_format = "cls %d len %d"% (class_id, len(centroids[class_id]))
