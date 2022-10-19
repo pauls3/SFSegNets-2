e8c4191f69b07270ce8ea6b2aa18d5365411964b
pauls3 <paulstanik3@gmail.com>
2022-10-19 01:54:52 -0700

    modified hyperparameters

--- scripts/rtisrail/train_rtisrail22_sfnet_res18.sh
+++ scripts/rtisrail/train_rtisrail22_sfnet_res18.sh
@@ -5,7 +5,7 @@ now=$(date +"%Y%m%d_%H%M%S")
 EXP_DIR=./logs/sfnet_resnet18_rtisrail22_pretrained_rs19_miou-0.753
 mkdir -p ${EXP_DIR}
 # Example on Cityscapes by resnet50-deeplabv3+ as baseline
-python3 -m torch.distributed.launch --nproc_per_node=4 train.py \
+python3 -m torch.distributed.launch --nproc_per_node=2 train.py \
   --dataset rtisrail22 \
   --cv 0 \
   --arch network.sfnet_resnet.DeepR18_SF_deeply \
@@ -27,9 +27,9 @@ python3 -m torch.distributed.launch --nproc_per_node=4 train.py \
   --bblur \
   --max_epoch 2000 \
   --wt_bound 1.0 \
-  --bs_mult 8 \
+  --bs_mult 4 \
   --apex \
-  --exp rtisrail22_SFnet_res18_lr_0.002 \
+  --exp rtisrail22_SFnet_res18 \
   --ckpt ${EXP_DIR}/ \
   --tb_path ${EXP_DIR}/ \
   --snapshot pretrained_models/best_epoch_391_mean-iu_0.75268.pth \
