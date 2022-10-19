2e40ecbcdbe377ee2c0d1e7255cc5f9da2282227
pauls3 <paulstanik3@gmail.com>
2022-10-19 01:46:59 -0700

    setting default key value

--- datasets/uniform.py
+++ datasets/uniform.py
@@ -176,7 +176,7 @@ def build_epoch(imgs, centroids, num_classes, class_uniform_pct):
 
     # now add uniform sampling
     for class_id in range(num_classes):
-        string_format = "cls %d len %d"% (class_id, len(centroids[class_id]))
+        string_format = "cls %d len %d"% (class_id, len(centroids[class_id, 0]))
         logging.info(string_format)
     for class_id in range(num_classes):
         centroid_len = len(centroids[class_id])
