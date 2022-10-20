baf27d636c5eb43dfda3da43e668c75fbe064699
pauls3 <paulstanik3@gmail.com>
2022-10-19 22:51:45 -0700

    enabled image dump

--- scripts/rtisrail/submit_testing.sh
+++ scripts/rtisrail/submit_testing.sh
@@ -9,5 +9,6 @@ python3 eval.py \
     --inference_mode whole \
     --scales 1 \
     --split test \
+    --dump_images \
     --ckpt_path logs/pretrained_railsem19_test_1 \
     --snapshot /home/stanik/repos/SFSegNets-3/SFSegNets-2/logs/sfnet_resnet18_rtisrail22_pretrained_rs19_miou-0.753/rtisrail22_SFnet_res18_test0/rtis-network.sfnet_resnet.DeepR18_SF_deeply_apex_T_bblur_T_bs_mult_4_class_uniform_pct_0.0_class_uniform_tile_1080_crop_size_1080_cv_0_dataset_rtisrai_lr_0.001_ohem_T_sbn/last_epoch_0_mean-iu_0.01083.pth
