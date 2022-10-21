d02e8d3baa618c5b77f322a102fc8730cd149a29
pauls3 <paulstanik3@gmail.com>
2022-10-20 21:42:18 -0700

    setting up new training session

--- scripts/rtisrail/train_rtisrail22_sfnet_stdc2_v2_1000e.sh
+++ scripts/rtisrail/train_rtisrail22_sfnet_stdc2_v2_1000e.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-#   --class_uniform_pct 0.5 \
-EXP_DIR=./sfnets/stdc2_SFV2_spatial_atten_rtisrail22
+EXP_DIR=./sfnets/stdc2_SFV2_spatial_atten
 mkdir -p ${EXP_DIR}
-python3 -m torch.distributed.launch --nproc_per_node=4 --master_port 29501  train.py \
-  --dataset rtisrail22 \
+python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 29501  train.py \
+  --dataset railsem19 \
   --cv 0 \
   --arch network.sfnet_stdcnet.AlignedSTDC2_SFV2_spatial_atten \
-  --class_uniform_pct 0.01 \
+  --class_uniform_pct 0.5 \
   --class_uniform_tile 1024 \
-  --lr 0.0015 \
+  --lr 0.0005 \
   --lr_schedule poly \
   --poly_exp 1.0 \
   --repoly 1.5  \
@@ -22,10 +21,10 @@ python3 -m torch.distributed.launch --nproc_per_node=4 --master_port 29501  trai
   --scale_max 2.0 \
   --color_aug 0.25 \
   --gblur \
-  --max_epoch 400 \
+  --max_epoch 2000 \
   --wt_bound 1.0 \
   --bs_mult 4 \
   --apex \
-  --exp rtisrail22_SFsegnet_stdc2 \
+  --exp railsem19_SFsegnet_stdc2 \
   --ckpt ${EXP_DIR}/ \
   --tb_path ${EXP_DIR}/
