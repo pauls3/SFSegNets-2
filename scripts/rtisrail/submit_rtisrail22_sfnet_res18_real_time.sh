eeea109d80cd9e71d8b8264db7ce3202bc84bcef
pauls3 <paulstanik3@gmail.com>
2022-10-22 02:07:13 -0700

    updated wrong script

--- scripts/rtisrail/submit_rtisrail22_sfnet_res18_real_time.sh
+++ scripts/rtisrail/submit_rtisrail22_sfnet_res18_real_time.sh
@@ -11,4 +11,4 @@ python3 eval.py \
     --split test \
      --dump_images \
     --ckpt_path logs/pretrained_railsem19_test_3 \
-    --snapshot pretrained_models/rtisrail22_sfnet_resnet18_mean-iu_0.88745.pth
+    --snapshot logs/sfnet_resnet18_rtisrail22_pretrained_rs19_miou-0.753/rtisrail22_SFnet_res18_train_3/rtis-network.sfnet_resnet.DeepR18_SF_deeply_apex_T_bblur_T_bs_mult_8_class_uniform_tile_1080_crop_size_1080_cv_0_dataset_rtisrai_lr_0.0005_ohem_T_PT_sbn/best_epoch_1496_mean-iu_0.87857.pth
