5f769464ab4988f8f11e9e42cd1eeef32444610d
pauls3 <paulstanik3@gmail.com>
2022-10-24 22:50:40 -0700

    timing only forward pass

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # echo "Running inference on" ${1}
 # echo "Saving Results :" ${2}
+
+
 # --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
 # --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
 # python3 demo_floder.py \
