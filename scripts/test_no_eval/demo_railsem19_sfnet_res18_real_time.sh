3dd09c2e6441c3e779672fde9efb1f2f726c5c51
pauls3 <paulstanik3@gmail.com>
2022-10-24 22:44:15 -0700

    doing another inference test

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -3,8 +3,15 @@
 # echo "Saving Results :" ${2}
 # --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
 # --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
+# python3 demo_floder.py \
+#     --arch network.sfnet_resnet.DeepR18_SF_deeply \
+#     --save_dir logs/rr22_dump_rs19_test/no_eval_test_0 \
+#     --snapshot pretrained_models/rtisrail22_sfnet_resnet18_mean-iu_0.88745.pth \
+#     --demo_folder /shared/rtis_lab/data/RailSem19/custom_split/test_images/
+
+
 python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --save_dir logs/rr22_dump_rs19_test/no_eval_test_0 \
-    --snapshot pretrained_models/rtisrail22_sfnet_resnet18_mean-iu_0.88745.pth \
-    --demo_folder /shared/rtis_lab/data/RailSem19/custom_split/test_images/
+    --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_3 \
+    --snapshot pretrained_models/railsem19_sfnet_resnet18_mean-iu_0.75268.pth \
+    --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images/
