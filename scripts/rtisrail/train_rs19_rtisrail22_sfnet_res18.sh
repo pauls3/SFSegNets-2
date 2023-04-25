677ad961d1de7fa75addd1e282aa5570bbe5548e
pauls3 <paulstanik3@gmail.com>
2023-04-25 14:49:54 -0700

    setting up training with trainVal and 700 epochs

--- scripts/rtisrail/train_rs19_rtisrail22_sfnet_res18.sh
+++ scripts/rtisrail/train_rs19_rtisrail22_sfnet_res18.sh
@@ -11,7 +11,7 @@ python3 -m torch.distributed.launch --nproc_per_node=4 train.py \
   --arch network.sfnet_resnet.DeepR18_SF_deeply \
   --class_uniform_pct 0.5 \
   --class_uniform_tile 1080 \
-  --lr 0.002 \
+  --lr 0.0025 \
   --lr_schedule poly \
   --poly_exp 1.0 \
   --repoly 1.5  \
@@ -25,7 +25,7 @@ python3 -m torch.distributed.launch --nproc_per_node=4 train.py \
   --color_aug 0.25 \
   --gblur \
   --bblur \
-  --max_epoch 500 \
+  --max_epoch 700 \
   --wt_bound 1.0 \
   --bs_mult 8 \
   --apex \
