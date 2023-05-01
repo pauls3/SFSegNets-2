4711afcb68953a981f62ef0d3e4831990d6d85c8
pauls3 <paulstanik3@gmail.com>
2023-04-30 21:20:35 -0700

    new exp

--- scripts/test_no_eval/demo_rs22_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_rs22_sfnet_res18_real_time.sh
@@ -14,6 +14,6 @@
 
 python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --save_dir logs/rr22_rs19_trainVal_test-no-eval_vid_3_good_tunnel \
+    --save_dir logs/rr22_rs19_trainVal_test-no-eval_vid_3_veg_people\
     --snapshot pretrained_models/rs19_rtisrail22_trainVal_mean-iu_0.69496.pth \
-    --demo_folder /home/stanik/repos/SFSegNets-3/SFSegNets-2/test_data/vid_3_good_tunnel/
+    --demo_folder /home/stanik/repos/SFSegNets-3/SFSegNets-2/test_data/vid_3_veg_people/
