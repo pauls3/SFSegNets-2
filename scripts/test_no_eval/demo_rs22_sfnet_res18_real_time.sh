1a234e605da383cfcb755a9304f8a9689f1a6843
pauls3 <paulstanik3@gmail.com>
2023-04-30 19:44:25 -0700

    next set for test

--- scripts/test_no_eval/demo_rs22_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_rs22_sfnet_res18_real_time.sh
@@ -16,4 +16,4 @@ python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
     --save_dir logs/rr22_rs19_trainVal_test-no-eval \
     --snapshot pretrained_models/rs19_rtisrail22_trainVal_mean-iu_0.69496.pth \
-    --demo_folder /home/stanik/repos/SFSegNets-3/SFSegNets-2/test_data/vid_0_mud_1/
+    --demo_folder /home/stanik/repos/SFSegNets-3/SFSegNets-2/test_data/vid_1_signs_0/
